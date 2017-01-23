0367caf96e6d1180d81a56cb66a669eac7de38cc
gerards <gerard@sharpe.net.au>
2017-01-23 22:55:02 +1100

    q7 toying with pprint

--- week1/q7_yaml_json_read.py
+++ week1/q7_yaml_json_read.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 import yaml, json
-from pprint import pprint
+import pprint
+
+pp = pprint.PrettyPrinter(indent=4)
 
 with open("q6_yaml_stuff.yml") as f:
     cisco_yaml = yaml.load(f)
@@ -10,7 +12,7 @@ with open("q6_json_stuff.json") as f:
     cisco_json = json.load(f)
 
 print("--- YAML ---")
-pprint(yaml.dump(cisco_yaml))
+pp.pprint(yaml.dump(cisco_yaml))
 
 print("--- JSON ---")
-pprint(json.dumps(cisco_json))
+pp.pprint(json.dumps(cisco_json))
