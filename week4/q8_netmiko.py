bf9291480f5362f944e76c01a3a5a6705525ece5
gerards <gerard@sharpe.net.au>
2017-03-06 22:43:21 +1100

    adding stuff

--- week4/q8_netmiko.py
+++ week4/q8_netmiko.py
@@ -1,38 +1,40 @@
 #!/usr/bin/env python
+'''
+Question from Pynet Network Automation course
+'''
 
-from netmiko import ConnectHandler 
-from getpass import getpass
+from netmiko import ConnectHandler
 
-config_file = 'q8_config_commands.txt'
-password = '88newclass' 
+CONFIG_FILE = 'q8_config_commands.txt'
+PASSWORD = '88newclass'
 
-pynet1 = {
-	'device_type': 'cisco_ios',
-	'ip': '184.105.247.70',
-	'username': 'pyclass',
-	'password': password,
-	'secret': '',
-	'port': 22
+PYNET1 = {
+    'device_type': 'cisco_ios',
+    'ip': '184.105.247.70',
+    'username': 'pyclass',
+    'PASSWORD': PASSWORD,
+    'secret': '',
+    'port': 22
 }
 
-pynet2 = {
-	'device_type': 'cisco_ios',
-	'ip': '184.105.247.71',
-	'username': 'pyclass',
-	'password': password,
-	'secret': '',
-	'port': 22
+PYNET2 = {
+    'device_type': 'cisco_ios',
+    'ip': '184.105.247.71',
+    'username': 'pyclass',
+    'PASSWORD': PASSWORD,
+    'secret': '',
+    'port': 22
 }
 
 
 if __name__ == '__main__':
-    pynet_rtr1 = ConnectHandler(**pynet1)
-    pynet_rtr2 = ConnectHandler(**pynet2)
-    
-    for rtr in pynet_rtr1, pynet_rtr2:
-        with open(config_file) as f:
+    PYNET_RTR1 = ConnectHandler(**PYNET1)
+    PYNET_RTR2 = ConnectHandler(**PYNET2)
+
+    for rtr in PYNET_RTR1, PYNET_RTR2:
+        with open(CONFIG_FILE) as f:
             print('*** ' + rtr.host + ' START ***')
-            print rtr.send_command('show run | in logging buffered')
-            print rtr.send_config_set(f)
-            print rtr.send_command('show run | in logging buffered')
+            print(rtr.send_command('show run | in logging buffered'))
+            print(rtr.send_config_set(f))
+            print(rtr.send_command('show run | in logging buffered'))
             print('*** ' + rtr.host + ' END ***')
