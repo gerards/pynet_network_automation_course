3d44600e06ed69840f494bd00190cfc2c7bf34f9
gerards <gerard@sharpe.net.au>
2017-08-29 20:07:54 +1000

    Bulk update

---
+++ tmp/week1q6.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+'''
+
+week1q6.py
+
+Write a program which:
+- Creates a list
+- The list should contain a dictionary with at least 2 keys
+- Write out list using YAML and JSON formats
+- YAML file needs to be in the expanded form
+
+'''
+
+
